e050da64e854bf87aa87ca6d9f766833d45046f4
e.m.breev <e.m.breev@gmail.com>
2019-09-05 00:18:22 +0300

    hw2

---
+++ hw2/script.js
@@ -0,0 +1,46 @@
+class GoodsItem {
+  constructor(title = 'No name', price = 'No price') {
+    this.title = title;
+    this.price = price;
+  }
+  render() {
+    return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+  }
+}
+
+class GoodsList {
+  constructor(container = '.container') {
+    this.container = container;
+    this.goods = [];
+  }
+  fetchGoods() {
+    this.goods = [
+      {
+        title: 'Shirt', price: 150
+      },
+      {
+        title: 'Socks', price: 50
+      },
+      {
+        title: 'Jacket', price: 350
+      },
+      {
+        title: 'Shoes', price: 250
+      },
+      {
+        price: 350
+      }
+    ];
+  }
+  render() {
+    document.querySelector(this.container).innerHTML = this.goods.reduce((acc, item) => {
+      const good = new GoodsItem(item.title, item.price);
+      return acc += good.render();
+    }, '');
+  }
+}
+
+
+const list = new GoodsList('.goods-list');
+list.fetchGoods();
+list.render();
