ceeb2de4efc5825fd3a792b9c5c229158857078e
e.m.breev <e.m.breev@gmail.com>
2019-09-07 13:44:27 +0300

    hw3

---
+++ hw3/script.js
@@ -0,0 +1,73 @@
+const BASE_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+function makeGETRequest(url, callback) {
+  const xhr = window.XMLHttpRequest ? new window.XMLHttpRequest() : new window.ActiveXObject('Microsoft.XMLHTTP');
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      callback(xhr.responseText)
+    }
+  };
+
+  xhr.open('GET', url);
+  xhr.send();
+}
+
+class GoodsItem {
+  constructor(title = 'No name', price = 'No price') {
+    this.title = title;
+    this.price = price;
+  }
+  render() {
+    return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+  }
+}
+
+class GoodsList {
+  constructor(container = '.container') {
+    this.container = container;
+    this.goods = [];
+  }
+  totalPrice() {
+    return this.goods.reduce((total, good) => {
+      if (!good.price) return total;
+      return total += good.price;
+    }, 0);
+  }
+  fetchGoods(cb) {
+    makeGETRequest(`${BASE_URL}/catalogData.json`, (goods) => {
+      this.goods = JSON.parse(goods);
+      cb();
+    });
+  }
+  render() {
+    document.querySelector(this.container).innerHTML = this.goods.reduce((acc, item) => {
+      const good = new GoodsItem(item.product_name, item.price);
+      return acc += good.render();
+    }, '');
+  }
+}
+
+class Cart extends GoodsList {
+  add(good) {}
+  remove(id) {
+    if (!id) {
+      // clean cart
+      return;
+    }
+  }
+  update(id, good) {}
+}
+
+class CartItem extends GoodsItem {
+  constructor(title = 'No name', price = 'No price', count = 1) {
+    super(title, price);
+    this.count = count;
+  }
+}
+
+
+const list = new GoodsList('.goods-list');
+list.fetchGoods(() => {
+  list.render();
+});
